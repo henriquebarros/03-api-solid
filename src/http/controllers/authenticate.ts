7556e18a2a7bf903bc062d042b6ea3de6c027824
henriquebarros <henriquebarros@live.com>
2024-03-10 12:38:22 -0300

    Controllers & Testes E2E - Implementando JWT no Fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -16,9 +16,22 @@ export const authenticate = async (
 
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
     })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
@@ -27,6 +40,4 @@ export const authenticate = async (
 
     throw err
   }
-
-  return reply.status(200).send()
 }
